722f33b14e0e893a238f6c86378e72a56a54cfa7
sirpolux <poluxreloaded@gmail.com>
2024-03-10 23:52:48 +0100

    feat subTopic list update

---
+++ src/components/chapter/SubTopic.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+
+const SubTopic = () => {
+    const subTopics={
+        "one":[
+            {
+                "topic":"Java Programming",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Setting up the development environment",
+                "exe":"0",
+                "duration": "5 mins"
+            },
+            {
+                "topic":"Basic strvture of Java Program",
+                "exe":"2",
+                "duration": "7 mins"
+            }
+        ],
+        "two":[
+            {
+                "topic":"Data Types",
+                "exe":"4",
+                "duration": "7 mins"
+            },
+            {
+                "topic":"Introduction to variables",
+                "exe":"3",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Arrays",
+                "exe":"5",
+                "duration": "8 mins"
+            }
+        ],
+        "three":[
+            {
+                "topic":"Assignment Operators",
+                "exe":"3",
+                "duration": "4 mins"
+            },
+            {
+                "topic":"Arithmetic Operators",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Logical Operators",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Relational Operators",
+                "exe":"4",
+                "duration": "6 mins"
+            }
+        ],
+        "four":[
+            {
+                "topic":"If Statements",
+                "exe":"4",
+                "duration": "7 mins"
+            },
+            {
+                "topic":"Switch statements",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Short Circuit Operators",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+        ],
+        "five":[
+            {
+                "topic":"For loops",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"While loops",
+                "exe":"2",
+                "duration": "6 mins"
+            },
+            {
+                "topic":"Do while loops",
+                "exe":"2",
+                "duration": "6 mins"
+            }
+        ],
+        "six":[
+            {
+                "topic":"Creating a function",
+                "exe":"6",
+                "duration": "8 mins"
+            },
+            {
+                "topic":"Calling a function",
+                "exe":"2",
+                "duration": "6 mins"
+            }
+        ]
+
+    }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SubTopic
--- src/components/chapter/SubTopicList.jsx
+++ src/components/chapter/SubTopicList.jsx
@@ -1,59 +1,8 @@
 import React from 'react'
 
 const SubTopicList = () => {
-    const subTopics={
-        "one":[
-            {
-                "topic":"Java Programming",
-                "exe":"2",
-                "duration": "6 mins"
-            },
-            {
-                "topic":"Setting up the development environment",
-                "exe":"0",
-                "duration": "5 mins"
-            },
-            {
-                "topic":"Basic strvture of Java Program",
-                "exe":"2",
-                "duration": "7 mins"
-            }
-        ],
-        "two":[
-            {
-                "topic":"Data Types",
-                "exe":"4",
-                "duration": "7 mins"
-            },
-            {
-                "topic":"Introduction to variables",
-                "exe":"3",
-                "duration": "6 mins"
-            },
-            {
-                "topic":"Arrays",
-                "exe":"5",
-                "duration": "8 mins"
-            }
-        ],
-        "three":[
-            {
-                "topic":"Assignment Operators",
-                "exe":"3",
-                "duration": "4 mins"
-            },
-            {
-                "topic":"Arithmetic Operators",
-                "exe":"2",
-                "duration": "6 mins"
-            }
-        ]
-    }
-
   return (
-    <div>
-
-    </div>
+    <div>SubTopicList</div>
   )
 }
 
