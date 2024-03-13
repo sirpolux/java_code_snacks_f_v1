219ffedadced885f3ad3d6b8f27133ddecd8106f
sirpolux <poluxreloaded@gmail.com>
2024-03-13 01:51:59 +0100

    feat added exampleList component

--- src/components/chapter/ExampleItem.jsx
+++ src/components/chapter/ExampleItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 
 
 const ExampleItem = () => {
-    const innerHtml ="<p className='text-red-500'> <p>"
+    const innerHtml = "<p class='text-blue-500 text-md'> Some for of sample text this is a sample data </p>"
+    
+    useEffect(()=>{
+        const target = document.querySelector('.target').innerHTML=innerHtml;
+    },[])
+   
+
   return (
     <div>
         <div className='bg-[#E7E9EB] p-3 pt-4'>
@@ -13,13 +20,15 @@ const ExampleItem = () => {
             </div>
             <div>
                 {/* content inner html to support formating */}
-                <div dangerouslySetInnerHTML={{ __html: innerHtml }}></div>
+                <div className='target bg-white p-3 mt-2 border-primary border-l-4'>
+                   
+                </div>
                 
             </div>
         </div>
         {/* comments on example */}
-        <div>
-
+        <div className='p-3 text-sm'>
+            <p>This section of code shows that it take a multiple of steps to achieve the common functionalities we will like to see in this project</p>
         </div>
     </div>
   )
